5516e183aed64dc0094b10d052b2289e2fd17108
Cole Chiga <colejchiga@gmail.com>
2023-09-26 14:20:13 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Cole Chiga";
+    int age = 25;
+    String state = "Colorado";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}
